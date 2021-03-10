502638ad9820cc8b5c0ca59f1f657638b7c18056
jegandran <51521640+jegandran@users.noreply.github.com>
2021-03-10 08:29:54 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import NavBar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 
-function App() {
-  return (
-   <Router>
-     <Sidebar />
-     <NavBar/>
-   </Router>
-  );
-}
+
+function App() {  
+    return (
+      <Router>
+        <Sidebar/>
+        <NavBar/>
+      </Router>
+    );
+  }
 
 export default App;
